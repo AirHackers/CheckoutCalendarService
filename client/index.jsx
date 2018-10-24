ae37c00423af905cb450bad8b78024329f15130a
Edmond Lee <techhexium@gmail.com>
2018-10-23 20:56:39 -0700

    Dev environment set up

---
+++ client/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CheckoutCalendar from './components/CheckoutCalendar.jsx';
+
+ReactDOM.render( <CheckoutCalendar />,
+document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client');
+var DIST_DIR = path.join(__dirname, '/public/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  devtool: 'source-map',
+  mode: 'development',
+  cache: true,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        include: SRC_DIR,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
