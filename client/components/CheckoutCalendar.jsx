f62f946ec990ddb7fbef212820c2a1682337094e
Edmond Lee <techhexium@gmail.com>
2018-10-26 11:39:44 -0700

    Started using compute endpoint to determine price

--- client/components/CheckoutCalendar.jsx
+++ client/components/CheckoutCalendar.jsx
@@ -5,12 +5,39 @@ import React from 'react';
 export default class CheckoutCalendar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      guests: 1,
+      days: 1,
+      price: null,
+      personPerNight: null
+    };
+  }
+
+  loadPrice(guests, days) {
+    fetch(`/api/compute?guests=${guests}&days=${days}`)
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        price: response.totalCost,
+        personPerNight: response.personPerNight
+      });
+    });
+  }
+
+  // Load default price for one person, for one day
+  componentDidMount() {
+    this.loadPrice(this.state.guests, this.state.days);
   }
 
   render() {
     return (
       <div className={this.props.small ? 'card container checkoutKeylinesTop' : 'card container'}>
-        <span className='checkoutKeylinesTop'><strong>$118</strong> per night</span> 
+        <span className='checkoutKeylinesTop'>
+        { this.state.price ?
+          <label><strong>${this.state.price}</strong> per night</label> : <label>Loading...</label>
+        }
+        </span>
 
         <hr />
         <label>Dates</label>
